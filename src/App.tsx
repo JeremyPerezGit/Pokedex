e2af82bfbc7fd43e05fe2542d89dcd5b1bbf3a5a
JeremyPerezGit <jeremy.perez13@hotmail.fr>
2024-10-22 17:47:12 +0200

    [FEAT] Nav buttons in the pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import "./App.css";
-
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const previousPokemon = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+  const nextPokemon = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[1]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 ? (
+        <button onClick={previousPokemon} type="button">
+          Précédent
+        </button>
+      ) : (
+        ""
+      )}
+      {pokemonIndex < pokemonList.length - 1 ? (
+        <button onClick={nextPokemon} type="button">
+          Suivant
+        </button>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
@@ -17,6 +39,21 @@ const pokemonList = [
     name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
     name: "mew",
