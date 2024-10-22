bb83c3b275e262bc2cec5a02abe74eb1709e0157
JeremyPerezGit <jeremy.perez13@hotmail.fr>
2024-10-22 16:33:46 +0200

    [FEAT] Type the props with interface

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import PokemonCard from "./components/PokemonCard";
 function App() {
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[1]} />
     </div>
   );
 }
